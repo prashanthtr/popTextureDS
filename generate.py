4279e171351d4e4310d43931e9532fdfc24e52de
prashanthtr <prashanth.thattai@gmail.com>
2020-11-23 15:50:55 +0800

    Implemented generic sound synthesizer

--- generate.py
+++ generate.py
@@ -5,33 +5,62 @@ import sys
 import itertools
 import numpy as np
 import os
-import pop_sound
+import soundfile as sf
+
 import librosa # conda install -c conda-forge librosa
 
+from pop_sound import soundModel
 from parammanager import paramManager
+from generic_sound import generic_synth
+
 #from Tf_record import tfrecordManager
+
+'''
+
+This code will generate a dataset of textures consiting of pops. A 'pop' is a burst of noise filtered by a bandpass filter. 
+
+The files are generated using 3 different parameters that are sampled over a range of values. The three parameters affect:
+    rate (average events per second), 
+    irregularity in temporal distribution (using a gaussian distribution around each evenly-spaced time value), and 
+    the center frequency of bp filter
+
+The parameter values are each sampled liniearly on an exponential scale:
+
+rate = 2^r_exp  (so r_exp in [0,4] means the rate ranges from 2 to 16)
+irregularity = .04*10^irreg_exp; sd = irregularity/events per second  (so irreg_exp in [0,1] means irregularity ranges from completely regular, to Poisson process)
+cf = 440*2^cf_exp  (so cf_exp in [0,1] means cf ranges from 440 to 880, one octave)
+
+For each parameter setting, first a "long" signal (of lentgth longDurationSecs) is generated, and then
+it is sliced into segments (called variations) of a length desired for training. 
+
+Example: If each parameter is sampled at 5 values, the long signal is 10 seconds and variationLength is 2 seconds,
+then The the total amount of audio generated is 5*5*5*10= 1250 seconds of sound (about 25 hours; ~3Gb at 16K sr).
+If each variation is 2 seconds, then there will be 10/2=5 variations for each parameter setting, and
+5*5*5*5 = 625 files
+'''
 
 paramArr = []
 data = []
 
-# inputs: file, folder?
+''' Initialize generic synthesizer that uses the sound model ''' 
+gs = generic_synth(soundModel)
+
 with open(sys.argv[1]) as json_file:
 	data = json.load(json_file)
 	print("Reading parameters for generating ", data['soundname'], " texture.. ")
 	for p in data['params']:
-	
-#	    print('Name: ' + p['pname'])
-#	    print('Units: ' + p['units']) 
-#	    print("Formula: " + p['formula'])
+		#	    print('Name: ' + p['pname'])
+		#	    print('Units: ' + p['units']) 
+		#	    print("Formula: " + p['formula'])
 	    p['formula'] = eval("lambda *args: " + p['formula'])
-#	    print('')
 	    paramArr.append(p)
+	gs.setParam(data['params'])
 
-# print("Cartesian products of linear interpolation of parameters.....")
+getParams = gs.getParam()
+print(getParams)
 
 cartParam = []
 
-# one way to generate parameters
 for p in paramArr:
 	cartParam.append(np.linspace(p["minval"], p["maxval"], p["nvals"], endpoint=True))
 
@@ -45,7 +74,6 @@ sr = data['samplerate']
 #data['soundname']
 
 # if directory exists, then ok, or else create relative to main directory
-
 filepath = data["outPath"]
 if os.path.isdir(filepath):
 	print("using filepath as outpath")
@@ -81,15 +109,20 @@ for enumP in enumParam: # caretesian product of lists
 	''' Construct variations filenames'''
 	for v in range(data['numVariations']):
 		vFilesWav.append(fname + '--v-'+'{:03}'.format(v)+'.wav') 
-		vFilesParam.append(fname + '--v-'+'{:03}'.format(v)+'.params') 
+		vFilesParam.append(fname + '--v-'+'{:03}'.format(v)+'.params')
 
 	''' Synthesize wav files'''
-	varDurationSecs=data["soundDuration"]/data["numVariations"]  #No need to florring this?
-	sig = pop_sound.generate_events(enumP, sr, data["soundDuration"])
+	varDurationSecs=data["soundDuration"]/data["numVariations"]  #No need to floor this?
+	sig = gs.synthesize(enumP, sr, data["soundDuration"])
 
 	''' Create param files '''
 	for v in range(data['numVariations']):
-		pop_sound.synthesize(sig, vFilesWav[v], outPath, sr, v, varDurationSecs)
+		
+		print("Printing", vFilesWav[v])
+		'''Create audio chunks for training'''
+		chunkedAudio = gs.selectVariation(sig, vFilesWav[v], outPath, sr, v, varDurationSecs)
+		sf.write(outPath + vFilesWav[v], chunkedAudio, sr)
+
 		pm=paramManager.paramManager(vFilesParam[v], outPath)
 		pm.initParamFiles(overwrite=True)
 		for pnum in range(len(paramArr)):
---
+++ generic_sound.py
@@ -0,0 +1,96 @@
+# Generic sound class to store sound synthesizer files
+import numpy as np
+import seaborn as sns
+from scipy import signal
+import math
+import sys
+
+''' Using a soundmodel and parameters, generic_Synth generates a texture
+
+setParam() list of parameters and their ranges
+
+synthesize() Generate events based on the current parameter values (rate, irregularity)
+
+getParams () To get the list of parameters and their ranges
+
+'''
+
+class generic_synth() :
+    
+    def __init__(self, soundModel) :
+        self.params = []
+        self.makeSingleEvent = soundModel # makes a single event
+        self.signal = [] # audio signal with sound events
+
+    def setParam(self, data):
+        for p in data:
+            print('Name: ' + p['pname'])
+            print('Units: ' + p['units'])
+            #print("Formula: " + p['formula'])
+            self.params.append(p)
+
+    def getParam(self):
+        return self.params
+
+    '''
+    Generic stochastic event generator with 3 parameters
+    Takes 3 parameters and produces sound events as specified by sound model
+    p1: rate, p2: irregularity, p3: cf
+    '''
+    def synthesize(self,parameters, sr, soundDurationSecs=4):
+        r_exp = parameters[0]
+        irreg_exp = parameters[1]
+        cf_exp = parameters[2]
+
+        # mapping to the right ranges
+        eps=np.power(2,r_exp)
+        irregularity=.1*irreg_exp*np.power(10,irreg_exp)
+        sd=irregularity/eps
+        cf=440*np.power(2,cf_exp)  #range over one octave from 440 to 880
+
+        linspacesteps=int(eps*soundDurationSecs)
+        linspacedur = linspacesteps/eps
+
+        eventtimes=[(x+np.random.normal(scale=sd))%soundDurationSecs for x in np.linspace(0, linspacedur, linspacesteps, endpoint=False)]
+
+        # Central frequency controls the Central frequency of bandpass filter
+        return self.elist2signal(eventtimes, soundDurationSecs, sr, cf,  50)
+
+    '''adds one (shorter) array in to another starting at startsamp in the second'''
+    def addin(self,a,b,startsamp) :
+        b[startsamp:startsamp+len(a)]=[sum(x) for x in zip(b[startsamp:startsamp+len(a)], a)]
+        return b
+
+    ''' Returns the signal starting sample number of the generated events. Visualization purposes'''
+    def generateEvents(self,myevents, soundDurSecs, numSamples, sr=16000):
+        sig=np.zeros(soundDurSecs*sr)
+        for nf in myevents :
+
+            #print("nf = {} and index is {}".format(nf, int(round(nf*sr))))
+            sig[int(round(nf*sr))%numSamples]=1
+        return sig
+
+    ''' Take a list of event times, and return our signal of filtered pops at those times'''
+    def elist2signal(self, elist, sigLenSecs, sr, cf, Q) :
+        numSamples=sr*sigLenSecs
+        sig=np.zeros(sigLenSecs*sr)
+        for nf in elist :
+            startsamp=int(round(nf*sr))%numSamples
+            # create some deviation in center frequency
+            cfsd = 1
+            perturbedCf = cf*np.power(2,np.random.normal(scale=cfsd)/12)
+            #print("perturbed CF is {}".format(perturbedCf))
+            #   pop(ticksamps, len, krms, f0, Q, fs)
+            sig=self.addin(self.makeSingleEvent(perturbedCf,Q, sr, 1000), sig,startsamp)
+        self.sig = sig
+        return sig
+
+    ''' Returns a chunked wav files from generated signal '''
+    def selectVariation(self, sig, fname, outDir, sr, varNum, varDurationSecs):
+
+        variationSamples=math.floor(sr*varDurationSecs)
+        print(len(self.sig[varNum*variationSamples:(varNum+1)*variationSamples]))
+        return self.sig[varNum*variationSamples:(varNum+1)*variationSamples]
+
+    def generateRandom(eventsPerSecond, sd, soundDurSecs, numSamples, sr=16000):
+        return [(x+np.random.normal(scale=sd))%soundDurSecs for x in np.linspace(0, soundDurSecs, eventsPerSecond*soundDurSecs, endpoint=False)]
